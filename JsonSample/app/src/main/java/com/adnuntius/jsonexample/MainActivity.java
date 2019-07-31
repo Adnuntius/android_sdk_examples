a40e11897397f181008093bce773b58baa416eac
Jason Pell <jason@adnuntius.com>
2019-07-31 19:02:17 +1000

    update ad view to not use static fields

--- JsonSample/app/src/main/java/com/adnuntius/jsonexample/MainActivity.java
+++ JsonSample/app/src/main/java/com/adnuntius/jsonexample/MainActivity.java
@@ -3,14 +3,51 @@ package com.adnuntius.jsonexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import com.adnuntius.sdk.lib.AdView;
 
+import com.adnuntius.android.sdk.AdView;
 
 public class MainActivity extends AppCompatActivity {
+    private AdView webView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        AdView.jsonConfig = "{\"siteId\": \"wsxcqfrnmxd95lm2\", \"adUnits\": [{\"auId\": \"0000000000023ae5\"}]}";
+
         setContentView(R.layout.activity_main);
+        this.webView = findViewById(R.id.adView);
+    }
+
+    @Override
+    protected void onResume() {
+       super.onResume();
+
+       webView.loadAdFromJson("{\"siteId\": \"wsxcqfrnmxd95lm2\", \"adUnits\": [{\"auId\": \"0000000000023ae5\"}]}");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        if (webView != null) {
+            webView.restoreState(savedInstanceState);
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        if (webView != null) {
+            webView.destroy();
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        if (webView != null) {
+            webView.saveState(outState);
+        }
     }
 }
