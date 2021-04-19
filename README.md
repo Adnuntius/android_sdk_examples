a36cf706468bc43a9fc81980e9af288223c7b2c4
Jason Pell <jason@adnuntius.com>
2021-04-19 14:08:14 +1000

    added extra data examples, updated the load Ad to use new api

--- README.md
+++ README.md
@@ -4,7 +4,8 @@ Example for android sdk
 
 ## Example
 
-Loading ads via loadFromConfig, loadFromApi, and using the Data SDK
+Loading ads via loadAd
+Samples of using the Data SDK
 
 # License
 
--- example/app/src/main/java/com/adnuntius/example/MainActivity.java
+++ example/app/src/main/java/com/adnuntius/example/MainActivity.java
@@ -6,12 +6,14 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
-import com.adnuntius.android.sdk.AdConfig;
+import com.adnuntius.android.sdk.AdRequest;
 import com.adnuntius.android.sdk.AdnuntiusAdWebView;
 import com.adnuntius.android.sdk.AdnuntiusEnvironment;
 import com.adnuntius.android.sdk.CompletionHandler;
 import com.adnuntius.android.sdk.data.DataClient;
 import com.adnuntius.android.sdk.data.DataResponseHandler;
+import com.adnuntius.android.sdk.data.Page;
+import com.adnuntius.android.sdk.data.Sync;
 import com.adnuntius.android.sdk.data.profile.Instant;
 import com.adnuntius.android.sdk.data.profile.LocalDate;
 import com.adnuntius.android.sdk.data.profile.Profile;
@@ -51,7 +53,6 @@ public class MainActivity extends AppCompatActivity {
         profile.setProfileValue(ProfileFields.country, "Norway");
         profile.setProfileValue(ProfileFields.dateOfBirth, LocalDate.of(1994, 2, 14));
         profile.setProfileValue(ProfileFields.createdAt, Instant.now());
-
         dataClient.profile(profile, new DataResponseHandler() {
             @Override
             public void onSuccess() {
@@ -64,34 +65,73 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        AdConfig config = new AdConfig("000000000006f450")
+        final Sync sync = new Sync();
+        sync.setFolderId("000000000000009d");
+        sync.setBrowserId("23123123132123123213213");
+        sync.setExternalSystemIdentifier("SOME SYSTEM", "adsdasdasd");
+        dataClient.sync(sync, new DataResponseHandler() {
+            @Override
+            public void onSuccess() {
+                Toast.makeText(getApplicationContext(),"Data Client Sync Success", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(ErrorResponse response) {
+                Log.d("dataClient", "Data Client Profile Sync Failure: " + response.getStatusCode());
+            }
+        });
+
+        final Page page = new Page();
+        page.setFolderId("000000000000009d");
+        page.setBrowserId("23123123132123123213213");
+        page.setDomainName("adnuntius.com");
+        page.addCategories("minecraft", "doom");
+        page.addKeywords("wood", "pla");
+        dataClient.page(page, new DataResponseHandler() {
+            @Override
+            public void onSuccess() {
+                Toast.makeText(getApplicationContext(),"Data Client Page Success", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(ErrorResponse response) {
+                Log.d("dataClient", "Data Client Profile Page Failure: " + response.getStatusCode());
+            }
+        });
+
+        AdRequest request = new AdRequest("000000000006f450")
+                .setWidth(300)
+                .setHeight(200)
+                .addKeyValue("version", "10");
+
+        adView.loadAd(request,
+                new CompletionHandler() {
+                    @Override
+                    public void onComplete(int adCount) {
+                        Toast.makeText(getApplicationContext(),"adView loadAd Success", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onFailure(String error) {
+                        Log.d("MainActivity.adView", "adView loadAd Failure: " + error);
+                    }
+                });
+
+        AdRequest request2 = new AdRequest("000000000006f450")
                 .setWidth(300)
                 .setHeight(200)
                 .addKeyValue("version", "4.3");
 
-        adView.loadFromConfig(config,
+        adView2.loadAd(request2,
                 new CompletionHandler() {
                     @Override
                     public void onComplete(int adCount) {
-                        Toast.makeText(getApplicationContext(),"loadFromConfig Success", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getApplicationContext(),"adView2 loadAd Success", Toast.LENGTH_SHORT).show();
                     }
 
                     @Override
                     public void onFailure(String error) {
-                        Log.d("MainActivity.adView", "loadFromConfig Failure: " + error);
-                    }
-                });
-
-        adView2.loadFromApi("{\"adUnits\": [{\"auId\": \"000000000006f450\", \"kv\": [{\"version\":\"10\"}]}]}",
-                new CompletionHandler() {
-                    @Override
-                    public void onComplete(int adCount) {
-                        Toast.makeText(getApplicationContext(),"loadFromApi Success", Toast.LENGTH_SHORT).show();
-                    }
-
-                    @Override
-                    public void onFailure(String error) {
-                        Log.d("MainActivity.adView2", "loadFromApi Failure: " + error);
+                        Log.d("MainActivity.adView2", "adView2 loadAd Failure: " + error);
                     }
                 });
     }
